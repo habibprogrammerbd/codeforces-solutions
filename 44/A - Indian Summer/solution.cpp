65d5a5ed22d85114b3e19e1a7ca738f6234dd8a7
Ahosan Habib <habibprogrammerbd@gmail.com>
2026-09-11 09:39:41 +0600

    Add Indian Summer [A] from Codeforces

---
+++ 44/A - Indian Summer/solution.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n;
+    cin >> n;
+    vector<pair<string, string>> v;
+    int con = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+ 
+        bool x = false;
+ 
+        string a, b;
+        cin >> a >> b;
+ 
+        for (int j = 0; j < v.size(); j++)
+        {
+            if (v[j].first == a && v[j].second == b)
+            {
+                x = true;
+                con++;
+            }
+        }
+ 
+        if (x == false)
+        {
+            v.push_back({a, b});
+        }
+    }
+ 
+  // habib
+    cout << n - con << endl;
+ 
+    return 0;
+}
