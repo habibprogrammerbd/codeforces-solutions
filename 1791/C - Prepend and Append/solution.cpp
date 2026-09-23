78ef4ec5a73558ec52ae1a35bb22c21119a2ef1b
Ahosan Habib <habibprogrammerbd@gmail.com>
2026-09-23 14:10:32 +0600

    Add second implementation for Prepend and Append
    Added a second implementation for the problem, handling input and processing logic for counting based on string conditions.

--- 1791/C - Prepend and Append/solution.cpp
+++ 1791/C - Prepend and Append/solution.cpp
@@ -1,3 +1,5 @@
+// frist inplation
+
 #include <bits/stdc++.h>
 using namespace std;
  
@@ -35,5 +37,80 @@ int main()
         cout << count << endl;
     }
  
+    return 0;
+}
+
+
+// secand impalition
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        int left = 0;
+        int right = s.size() - 1;
+
+        int count = 0;
+        bool x = false;
+        while (left <= right)
+        {
+            if (x == false)
+            {
+                if (s[left] == '0' && s[right] == '1')
+                {
+                    left++;
+                    right--;
+                }
+                else if (s[left] == '1' && s[right] == '0')
+                {
+                    left++;
+                    right--;
+                }
+                else
+                {
+                    x = true;
+                    if (left == right)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count += 2;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+            else
+            {
+                if (left == right)
+                {
+                    count++;
+                }
+                else
+                {
+                    count += 2;
+                }
+                left++;
+                right--;
+            }
+        }
+
+        cout << count << endl;
+    }
+
     return 0;
 }
