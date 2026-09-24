7b355ef453d7579034cba2b2d1d204bf59f31ced
Ahosan Habib <habibprogrammerbd@gmail.com>
2026-09-24 18:10:32 +0600

    Add Prepend and Append [C] from Codeforces

--- 1791/C - Prepend and Append/solution.cpp
+++ 1791/C - Prepend and Append/solution.cpp
@@ -1,5 +1,3 @@
-// frist inplation
-
 #include <bits/stdc++.h>
 using namespace std;
  
@@ -20,49 +18,6 @@ int main()
         int left = 0;
         int right = s.size() - 1;
  
-        int count = n;
-        while (left <= right)
-        {
-            if (s[left] == s[right])
-            {
-                break;
-            }
-            else
-            {
-                count -= 2;
-                left++,right--;
-            }
-        }
- 
-        cout << count << endl;
-    }
- 
-    return 0;
-}
-
-
-// secand impalition
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        string s;
-        cin >> s;
-
-        int left = 0;
-        int right = s.size() - 1;
-
         int count = 0;
         bool x = false;
         while (left <= right)
@@ -108,9 +63,9 @@ int main()
                 right--;
             }
         }
-
+ 
         cout << count << endl;
     }
-
+ 
     return 0;
 }
